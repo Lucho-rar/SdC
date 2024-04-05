61fbb3cd42bed86fbbc7e947de6bb5ac138f5e5c
RoblesYesica <karen.yesica.robles@gmail.com>
2024-04-05 20:01:55 -0300

    agrega programa en c

--- practico2/gini.py
+++ practico2/gini.py
@@ -1,8 +1,13 @@
-#Author: Robles, Karen Yesica;Rodriguez Luciano;
+#Author: Robles, Karen Yesica;Rodriguez Luciano;Magin Matias
 #Version: 1.0.0
 
 import requests
 import json
+import ctypes
+
+libsuma = ctypes.CDLL('./libsuma.so')
+libsuma._suma.argtypes = [ctypes.c_float]
+libsuma._suma.restype = ctypes.c_int
 
 def show_menu():
 	print("1. Select pais")
@@ -12,17 +17,20 @@ def get_url(country):
     url = f"https://api.worldbank.org/v2/en/country/{country}/indicator/SI.POV.GINI?format=json&date=2011:2020&per_page=32500&page=1"
     return url
 
+def get_suma(num):
+	return libsuma._suma(num)
+
 def option_1():
 	 country = input ("Please enter a string:")
-	 #print(country)
 	 url = get_url(country)
 	 response = requests.get(url)
 	 data = response.json()
 	 elementos = data[1]
 	 for elem in elementos:
 	     element = elem.get('value')
+	     num = get_suma(element) if element is not None else "None"
 	     dat = elem.get('date')
-	     print (f"Date : {dat} Indice GINI : {element}") 
+	     print (f"Date : {dat} Indice GINI : {element} ---> {num}")
 
 
 while True:
---
+++ practico2/suma.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+//aqui protipo de la funcion assembler
+int _suma(float index){
+    //aqui convertir el float a entero en assembler
+    int index_gini = (int) index;
+	return index_gini+1;
+}
